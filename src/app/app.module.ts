4e64bcfe48d8abd0dccdb493f441fc9541842c7b
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-03-09 15:55:43 +0330

    till datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './auth/home/home.component';
 import { TrainingComponent } from './training/training.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,16 @@ import { TrainingComponent } from './training/training.component';
     HomeComponent,
     TrainingComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+  ],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-
+  onSubmit(f: NgForm) {
+    console.log(f);
+  }
 }
