d46ec67d3276e9b947f2b23b429b3c3e7b97ba2a
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-03-13 15:22:50 +0330

    read exercises from model

--- src/app/new-training/new-training.component.ts
+++ src/app/new-training/new-training.component.ts
@@ -1,12 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { TrainingService } from '../training/training.service';
+import { Exercise } from '../training/exercise.model';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrl: './new-training.component.css',
 })
-export class NewTrainingComponent {
+export class NewTrainingComponent implements OnInit {
   @Output() startTrain = new EventEmitter<void>();
+  exercises: Exercise[] | undefined;
+
+  constructor(private training: TrainingService) {}
+
+  ngOnInit() {
+    this.exercises = this.training.getAvailableExercises();
+    console.log(this.exercises);
+  }
 
   starttrain() {
     this.startTrain.emit();
---
+++ src/app/training/training.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Exercise } from './exercise.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrainingService {
+  constructor() {}
+
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'کرانچ', duration: 30, calories: 8 },
+    { id: 'squat', name: 'اسکوات', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'لانچ اسکوات', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'شنا', duration: 60, calories: 8 },
+  ];
+
+  getAvailableExercises() {
+    return this.availableExercises.slice();
+  }
+}
