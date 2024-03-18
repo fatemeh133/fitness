181a640cece3429d06036f9caffc6cc64acf5f1c
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-03-18 17:40:12 +0330

    search input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,6 +13,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   imports: [
@@ -31,6 +32,7 @@ import { MatTableModule } from '@angular/material/table';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
+    MatSortModule,
   ],
   exports: [
     MatSlideToggleModule,
@@ -48,6 +50,7 @@ import { MatTableModule } from '@angular/material/table';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
+    MatSortModule,
   ],
 })
 export class MaterialModule {}
--- src/app/past-training/past-training.component.ts
+++ src/app/past-training/past-training.component.ts
@@ -1,12 +1,21 @@
+import { MatTableDataSource } from '@angular/material/table';
 import { TrainingService } from './../training/training.service';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
+import { Exercise } from '../training/exercise.model';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrl: './past-training.component.css',
 })
-export class PastTrainingComponent {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
     'name',
     'duration',
@@ -14,8 +23,19 @@ export class PastTrainingComponent {
     'date',
     'state',
   ];
+  dataSource = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private trainingService: TrainingService) {}
 
-  dataSource = this.trainingService.getCompeletedExercise();
+  ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompeletedExercise();
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
